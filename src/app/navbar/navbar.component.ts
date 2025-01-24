cf74a0cc2f17ab85228be4502e45b89b6920fe01
preyash <phojiwala.techark@gmail.com>
2025-01-24 12:22:41 +0530

    chat page & com added

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  links = [
+    {
+      link: '/posts',
+      label: 'Posts'
+    },
+    {
+      link: '/users',
+      label: 'Users'  
+    },
+    {
+      link: '/chat',
+      label: 'Chat'  
+    }
+  ];
 }
--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ import { PostsComponent } from './app/posts/posts.component';
 import { UsersComponent } from './app/users/users.component';
 import { PostService } from './app/posts/post.service';
 import { UserService } from './app/users/user.service';
+import { ChatComponent } from './app/chat/chat.component';
 
 const routes: Routes = [
   { path: 'posts', component: PostsComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'chat', component: ChatComponent },
   { path: '', redirectTo: '/posts', pathMatch: 'full' }
 ];
 
